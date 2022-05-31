c8d9dea87a54b92f8742ea0b6549e4d7f941e5a7
wuxinkai <275648208@qq.com>
2022-05-31 15:29:52 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import router from './router/router.js'; // 路由
 Vue.config.productionTip = false
-
 new Vue({
-  router,
   render: function (h) {
     return h(App)
   }
